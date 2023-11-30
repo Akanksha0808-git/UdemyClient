9532664c02f6a880f07c1ffe091559645147830a
Akanksha <akanksharma0829@gmail.com>
2023-11-30 19:22:18 +0530

    client side

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import RouteFiles from "./Components/RouteCompo/RouteFiles"
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    
+      <RouteFiles/> 
+    </div>
+  )
+}
+
+export default App
